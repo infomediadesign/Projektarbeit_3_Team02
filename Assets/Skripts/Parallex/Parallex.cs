b6ce7f5e5629c253ebbdf516ce8c159cd356c9f8
Lara <152970053+LaraRF@users.noreply.github.com>
2024-11-05 18:17:12 +0100

    P- parallex update

--- Assets/Skripts/Parallex/Parallex.cs
+++ Assets/Skripts/Parallex/Parallex.cs
@@ -17,7 +17,11 @@ public class Parallex : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
         float dist = (cam.transform.position.x * parallexEffect);
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if(temp < startpos - length) startpos -= length;
     }
 }
