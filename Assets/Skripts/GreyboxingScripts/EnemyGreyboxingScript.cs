a09b1dd93936c7a3356b18ba4d43046ec7a5b787
Chimaera39 <Leon.Tschurtschenthaler@stud.Hochschule-Heidelberg.de>
2024-11-20 20:12:23 +0100

    D - Enemy und Projectile Prefabs - GreyboxingPrefabs Ordner

---
+++ Assets/Skripts/GreyboxingScripts/EnemyGreyboxingScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyGreyboxingScript : MonoBehaviour
+{
+    public int enemyType = 1;
+    public bool isPatrolling = false;
+    public Vector2 patrolStarPos = Vector2.zero;
+    public Vector2 patrolEndPos = Vector2.zero;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Skripts/GreyboxingScripts/ProjectileSpawnerGreyboxingScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ProjectileSpawnerGreyboxingScript : MonoBehaviour
+{
+    public Vector2 direction = Vector2.down;
+    public int cooldown = 4;
+    public int startOffset = 0;
+    public int numberOfShots = 1;
+    public int timeBetweenShots = 4;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
