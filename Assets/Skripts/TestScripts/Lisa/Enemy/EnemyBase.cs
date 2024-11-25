aaae4838f4db2f6820a4dd8c72359e97261d6865
lisalylo <11037798@stud.hochschule-heidelberg.de>
2024-11-25 15:11:29 +0100

    BaseEnemy und EnemyStats entwurf

---
+++ Assets/Skripts/TestScripts/Lisa/Enemy/EnemyBase.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    public EnemyStats stats;
+    protected float currentHealth;
+    protected virtual void Start()
+    {
+        currentHealth = stats.maxHealth;
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0) Die();
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public abstract void Attack();
+}
