cd27745c9da6da4b03a8580946604e24496164bc
sadehoskins <152971401+sadehoskins@users.noreply.github.com>
2024-12-10 15:30:41 +0100

    Persistent Data Scene
    Handles all persistent data: camera, player

---
+++ Assets/Skripts/TestScripts/Sade/Persistence.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Persistence : MonoBehaviour
+{
+    
+    public static Persistence instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
