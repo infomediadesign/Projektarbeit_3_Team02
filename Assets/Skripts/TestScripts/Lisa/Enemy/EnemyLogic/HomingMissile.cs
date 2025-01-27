1ac342bc3fcfac620e4d50dcce1b14de7845ada7
lisalylo <11037798@stud.hochschule-heidelberg.de>
2025-01-27 17:00:53 +0100

    boss

--- Assets/Skripts/TestScripts/Lisa/Enemy/EnemyLogic/HomingMissile.cs
+++ Assets/Skripts/TestScripts/Lisa/Enemy/EnemyLogic/HomingMissile.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HomingMissile : MonoBehaviour
 {
-    public Transform target;
+    public Transform player;
     private Rigidbody2D rb;
     public int damage = 10;
     public float speed = 5f;
@@ -12,9 +12,18 @@ public class HomingMissile : MonoBehaviour
     private bool isFollowing = true;
     private float followTimer;
     private Vector2 currentDirection;
+    private float input;
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            if (playerObject != null)
+            {
+                player = playerObject.transform;
+            }
+        }
+        //target = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
         followTimer = followDuration;
         currentDirection = transform.up;
@@ -30,26 +39,40 @@ public class HomingMissile : MonoBehaviour
             if (followTimer <= 0f)
             {
                 isFollowing = false; // timer abgelaufen, verfolgen beenden
-                currentDirection = transform.up;
                 rb.angularVelocity = 0;
             }
         }
+        input = Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate()
     {
-        if (isFollowing)
+        /*if (isFollowing)
         {
-            Vector2 direction = ((Vector2)target.position - rb.position).normalized;
+            Vector2 direction = ((Vector2)player.position - rb.position).normalized;
             float rotateAmount = Vector3.Cross(direction, transform.up).z;
 
             rb.angularVelocity = -rotateAmount * rotateSpeed; // drehen in zielrcihtung
-            rb.velocity = transform.up * speed;
+            rb.linearVelocity = transform.up * speed;
             currentDirection = rb.linearVelocity.normalized;
         }
         else
         {
-            rb.velocity = currentDirection * speed;
+            rb.linearVelocity = currentDirection * speed;
+        }*/
+        if (isFollowing)
+        {
+            rb.linearVelocity = transform.up * speed * Time.deltaTime * 10f;
+            Vector2 direction = (player.transform.position - transform.position).normalized;
+            float rotationSteer = Vector3.Cross(transform.up, direction).z;
+            rb.angularVelocity = rotationSteer * rotateSpeed * 10f;
+            currentDirection = direction;
+        }
+        else
+        {
+            rb.linearVelocity = transform.up * speed * Time.deltaTime * 10f;
+            float rotationSteer = Vector3.Cross(transform.up, currentDirection).z;
+            rb.angularVelocity = rotationSteer * rotateSpeed * 10f;
         }
     }
 
