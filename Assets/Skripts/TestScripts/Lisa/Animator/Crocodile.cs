a0aae98e93dc583482cc82ef9fd7af4b5d74199e
lisalylo <11037798@stud.hochschule-heidelberg.de>
2025-03-10 14:13:50 +0100

    crocodile updates

--- Assets/Skripts/TestScripts/Lisa/Animator/Crocodile.cs
+++ Assets/Skripts/TestScripts/Lisa/Animator/Crocodile.cs
@@ -8,7 +8,6 @@ public class Crocodile : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animFinished = false;
         anim = GetComponent<Animator>();
     }
 
