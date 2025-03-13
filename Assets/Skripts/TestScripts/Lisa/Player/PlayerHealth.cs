692fe57b545594f2324002299cc063f20f390bc3
lisalylo <11037798@stud.hochschule-heidelberg.de>
2025-03-13 11:29:03 +0100

    dmg anim thorns and block

--- Assets/Skripts/TestScripts/Lisa/Player/PlayerHealth.cs
+++ Assets/Skripts/TestScripts/Lisa/Player/PlayerHealth.cs
@@ -33,13 +33,17 @@ public class PlayerHealth : MonoBehaviour
     {
         if(currentHealth > 0)
         {
-            state.damageAnim = true;
-            if (!state.shielded && currentHealth > 0 && !state.rolling)
+            if (!state.shielded)
             {
-                currentHealth -= damage;
-                // Event auslösen, wenn sich die Gesundheit ändert
-                EventManager.Instance.TriggerEvent<int>("HealthChanged", (int)currentHealth);
+                state.damageAnim = true;
+                if (currentHealth > 0 && !state.rolling)
+                {
+                    currentHealth -= damage;
+                    // Event auslösen, wenn sich die Gesundheit ändert
+                    EventManager.Instance.TriggerEvent<int>("HealthChanged", (int)currentHealth);
+                }
             }
+         
         }
              
         if (currentHealth <= 0)
